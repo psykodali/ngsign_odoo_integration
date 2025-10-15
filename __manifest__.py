cb57e5d4d2d0b0a206a6cc53a8e38948ec720000
Dali <psykodali@gmail.com>
2025-10-15 12:16:05 +0100

    RC1

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    "name": "NGSIGN Integration V4",
+    "name": "NGSIGN Integration",
     "version": "18.0.1.0.0",
     "summary": "Send Quotation for signature with NGSIGN.",
     "author": "NGSign",
